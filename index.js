40c2f2d6057e994d484806fbaab57f34fb7f268c
Emilly Agatha <135267138+emyzou@users.noreply.github.com>
2023-08-23 17:13:49 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,111 @@
+/*
+let a = 3;
+let b = 5;
+let c = 6;
+let d = 7;
+let e = 4;
+
+console.log(a % 2);
+console.log(b % 2);
+console.log(c % 2);
+console.log(d % 2);
+console.log(e % 2);
+*/
+//minhaIdade
+/*
+let minhaIdade = 26;
+console.log("Antes", minhaIdade);
+minhaIdade = minhaIdade + 1;
+console.log("Depois", minhaIdade);
+*/
+/*
+let numero1 = 3;
+let numero2 = 4;
+console.log(numero1 + numero2);
+
+//Como eu fiz
+let multiplicacao = 3 * 5;
+console.log(multiplicacao / 2);
+
+let subtraia = 5 - 4;
+console.log(subtraia * -1);
+
+let divisao = 234 / 5;
+console.log(divisao % 5);
+ 
+//Profesor fez
+let c = 3;
+let d = 5;
+let e = 2;
+let f = -1;
+console.log(c * d / 2);
+console.log((d - 8) * f);
+let g = 234;
+let h = 5;
+console.log(g % h);
+*/
+
+//exercicio2
+/*
+let a = 10;
+let b = 20;
+console.log(a === b);
+console.log(a !== b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+*/
+
+//comparadores logicos
+/*
+let a = 5;
+let b = 3;
+let c = 0;
+console.log(a === b || a !== c);
+console.log(a === b && a !== c);
+console.log("---- separador ----");
+
+console.log(true || false);
+console.log(true && false);
+console.log(true && true);
+*/
+
+//exercicio 3
+/*
+let a = true;
+let b = false;
+let c = true;
+console.log(a && b);
+console.log(b && c);
+console.log(a && c);
+console.log(a && b && c);
+
+console.log( a || b);
+console.log( b || c);
+console.log( a || c);
+console.log( a || b || c);
+*/
+
+//exercicio 4 - negacao
+/*
+let a = true;
+let b = false;
+
+console.log(a);
+console.log(!a);
+
+console.log(b);
+console.log(!b);
+*/
+
+//exercicio 5
+/*
+let nome = "Laís";
+let idade = 23;
+
+console.log("Nome:", nome);
+console.log("Idade:", Number (idade));
+console.log("É maior de idade?", idade > 18);
+console.log("Idade em 2050:", 2050 - 1998);
+*/
+
